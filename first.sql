4d48a23685cc3fba282e5f06d6aa25c734255a5d
yusufk999 <mdyusufk999@gmail.com>
2023-05-12 14:31:09 +0530

    Added additional queries

---
+++ first.sql
@@ -0,0 +1,33 @@
+create database temp;
+
+use temp;
+
+create table student(
+id INT PRIMARY KEY,
+name VARCHAR(255)
+);
+
+insert into student values (1, 'Ankit');
+show tables;
+select * from student;
+
+drop database if exists temp;
+
+
+create database org;
+
+use org;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
